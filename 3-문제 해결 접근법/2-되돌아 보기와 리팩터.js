9292e84e262528d771a98aa03605ed701da05201
Min <esspacetoM@gmail.com>
2023-02-20 20:51:28 +0900

    문제 해결 접근법

---
+++ 3-문제 해결 접근법/2-되돌아 보기와 리팩터.js
@@ -0,0 +1,18 @@
+const charrCount = (str) => {
+  let result = {};
+
+  for (let char of str) {
+    char = char.toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (result[char] > 0) {
+        result[char]++;
+      } else {
+        result[char] = 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+console.log(charrCount("Hi hello"));
