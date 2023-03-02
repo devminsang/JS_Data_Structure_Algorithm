8f281703c2f55d68c76cae9eb50134adc791e41f
Min <esspacetoM@gmail.com>
2023-03-02 17:58:53 +0900

    Update 5-maxSubarraySum.js

--- 5-도전 과제/5-maxSubarraySum.js
+++ 5-도전 과제/5-maxSubarraySum.js
@@ -7,26 +7,27 @@ const maxSubarraySum = (arr, n) => {
   let i = 0;
   let j = 1;
   let cnt = 1;
-  let temp = 0;
+  let temp = arr[0];
 
   while (j <= arr.length) {
-    console.log(i, j);
-    if (j < i + n) {
-      temp += arr[j];
-      j++;
-    } else {
+    if (cnt === n) {
+      cnt = 1;
       i++;
       j = i + 1;
       answer.push(temp);
-      temp = 0;
+      temp = arr[i];
+    } else {
+      temp += arr[j];
+      j++;
+      cnt++;
     }
   }
 
-  console.log(answer);
+  return Math.max(...answer);
 };
 
-maxSubarraySum([100, 200, 300, 400], 2); // 700
-maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4); // 39
-maxSubarraySum([-3, 4, 0, -2, 6, -1], 2); // 5
-maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2); // 5
-maxSubarraySum([2, 3], 3); // null
+console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)); // 5
+console.log(maxSubarraySum([2, 3], 3)); // null
